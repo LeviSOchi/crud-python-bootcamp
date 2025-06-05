57c611ffd871846fa2e1a5b236bc9dc32b53bfcf
Levi Ochi <leviochi12@gmail.com>
2025-06-05 20:03:34 -0300

    database update

--- backend/database.py
+++ backend/database.py
@@ -6,12 +6,12 @@ POTSGRES_DATABASE_URL = "postgres://user:password@postgres/mydatabase"
 
 engine = create_engine(POTSGRES_DATABASE_URL)
 
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 def get_db():
-    db = sessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
